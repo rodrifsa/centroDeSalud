8475969abc3602d7fbc11cea0fab4dd0d3db552a
Guillermo Gonzalez <2808guille@gmail.com>
2024-07-22 17:08:35 -0300

    Carpeta Pacientes

---
+++ pacientes/medicos/actualizarpaciente.php
@@ -0,0 +1,37 @@
+<?php
+
+    $ID  = $_REQUEST['ID'];
+    $cnombre_medico  = strtoupper($_REQUEST['cnombre_medico']);
+    $ndni_medico  = strtoupper($_REQUEST['ndni_medico']);
+    $cdireccion_medico  = strtoupper($_REQUEST['cdireccion_medico']);
+    $ctelefono_medico  = strtoupper($_REQUEST['ctelefono_medico']);
+    $nmatricula_medico  = strtoupper($_REQUEST['nmatricula_medico']);
+    $id_especialidad  = strtoupper($_REQUEST['id_especialidad']);
+    $dfecha_inicio  = strtoupper($_REQUEST['dfecha_inicio']);
+    
+    //conectar a la base de datos
+    $db_servidor ="127.0.0.1"; //es lo mismo que localhost
+    $db_usuario = "root";
+    $db_password= "";
+    $db_basededatos ="centro_de_salud";
+
+    $conexion = mysqli_connect($db_servidor, $db_usuario, $db_password, $db_basededatos)
+        or die("NO SE PUDO CONECTAR A LA BASE DE DATOS");
+
+  
+    //preparo la actualizacion 
+    $consulta ="UPDATE medicos SET cnombre_medico = '$cnombre_medico',
+        ndni_medico= $ndni_medico,
+        cdireccion_medico = '$cdireccion_medico',
+        ctelefono_medico = '$ctelefono_medico',
+        nmatricula_medico = $nmatricula_medico,
+        id_especialidad = $id_especialidad,
+        dfecha_inicio = '$dfecha_inicio' 
+        WHERE id=" . $ID;
+    
+    //realizar consulta a la tabla especielidades
+    $respuesta = mysqli_query( $conexion, $consulta ) 
+        or die("ERROR EN LA CONSULTA");
+
+    header("location: medicos.php");
+    ?>
---
+++ pacientes/medicos/nuevopaciente.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles.css">
+
+    <title>CENTRO DE SALUD - NUEVO MEDICO</title>
+</head>
+
+<body>
+
+    <?php
+
+    $error="";
+
+    if(isset($_REQUEST['error'])){
+        $error=$_REQUEST['error'];
+    }
+
+    //header de la pagina
+
+    include('../plantillas/header.php');
+
+    echo "<center><u><h2>NUEVO MEDICO</h2></u>";
+
+    //conectar a la base de datos
+    $db_servidor ="127.0.0.1"; //es lo mismo que localhost
+    $db_usuario = "root";
+    $db_password= "";
+    $db_basededatos ="centro_de_salud";
+
+    $conexion = mysqli_connect($db_servidor, $db_usuario, $db_password, $db_basededatos)
+        or die("NO SE PUDO CONECTAR A LA BASE DE DATOS");
+
+
+    //preparo la consulta 
+    $consulta ="SELECT id, cnombre_especialidad FROM especialidades";
+    
+    //realizar consulta a la tabla especielidades
+    $respuesta = mysqli_query( $conexion, $consulta ) 
+        or die("ERROR EN LA CONSULTA");
+
+    ?>
+    <center>
+        <Form action="guardarmedico.php" method="POST">
+
+            <!-- nombre del medico -->
+            <div class="input-form">
+                <label>Nombre :</label>
+                <input type="text" name="cnombre_medico" size="50" minlength="5" required>
+
+                <?php 
+                    if($error<>""){
+                        echo" <br><span style='color:red'>$error</span>";
+                    }
+                ?>
+            </div>
+
+           <!-- dni del medico --> 
+            <div class="input-form">
+                <label>Dni :</label>
+                <input type="number" name="ndni_medico" size="8" required>
+
+                <?php 
+                    if($error<>""){
+                        echo" <br><span style='color:red'>$error</span>";
+                    }
+                ?>
+            </div>
+
+            <!-- direccion del medico -->
+            <div class="input-form">
+                <label>Direci&oacute;n :</label>
+                <input type="text" name="cdireccion_medico" size="60" required >
+
+                <?php 
+                    if($error<>""){
+                        echo" <br><span style='color:red'>$error</span>";
+                    }
+                ?>
+            </div>
+
+            <!-- telefono del medico -->
+            <div class="input-form">
+                <label>Tel&eacute;fono :</label>
+                <input type="text" name="ctelefono_medico" size="50" required>
+
+                <?php 
+                    if($error<>""){
+                        echo" <br><span style='color:red'>$error</span>";
+                    }
+                ?>
+            </div>
+
+            <!-- especialidad del medico --> 
+            <div class="input-form">
+                <label>Especialidad :</label>
+                <select name="id_especialidad">
+                
+                <?php 
+                    //recorrer la respuesta
+                    while($row = mysqli_fetch_assoc($respuesta)){
+
+                            echo "<option value='" . $row['id'] . "'>" . $row['cnombre_especialidad'] ."</option>"; 
+                    }   
+                ?> 
+                </select>
+
+              
+                <?php 
+                    if($error<>""){
+                        echo" <br><span style='color:red'>$error</span>";
+                    }
+                ?>
+            </div>
+
+             <!-- matricula del medico --> 
+             <div class="input-form">
+                <label>Matricula :</label>
+                <input type="number" name="nmatricula_medico" size="10" required>
+
+                <?php 
+                    if($error<>""){
+                        echo" <br><span style='color:red'>$error</span>";
+                    }
+                ?>
+            </div>
+
+            <!-- inicio de actividades --> 
+            <div class="input-form">
+                <label>Fecha de Inicio :</label>
+                <input type="date" name="dfecha_inicio">
+               
+                <?php 
+                    if($error<>""){
+                        echo" <br><span style='color:red'>$error</span>";
+                    }
+                ?>
+            </div>
+
+            <br>
+            <div class="input-form">
+                <input type="submit" value="Guardar">
+            </div>
+        </Form>
+
+    </center>
+</body>
+
+</html>
